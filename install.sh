00aa8132a65d0be1aa3fe409910fab82693329ea
Lionel T <elrumordelaluz@hotmail.com>
2014-12-09 21:47:33 +0100

    install file

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Starting the installation..."
+
+bower install git://github.com/elrumordelaluz/template.git
+
+mv bower_components/Template\ base/* .
+
+sudo npm install gulp --save-dev
+
+sudo npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache --save-dev
+
+rm -r bower_components/
+
+rm bower.json 
+
+echo "Installation finished!"
