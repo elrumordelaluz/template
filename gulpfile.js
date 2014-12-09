a3c1cbdb8768d82886891ed8fb717b2021d2a1b2
Lionel T <elrumordelaluz@hotmail.com>
2014-12-09 18:10:59 +0100

    gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function(){
     return gulp.src('src/styles/*.scss')
-        .pipe(sass({ "sourcemap=none": true, style: 'expanded' }))
+        .pipe(sass({ style: 'expanded' }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .on('error', function (err) { console.log(err.message); })
         .pipe(gulp.dest('dist/assets/css'))
