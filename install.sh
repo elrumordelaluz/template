58d5ef10aea5a1d11983d5c7d6402adc7721f501
Lionel T <elrumordelaluz@hotmail.com>
2014-12-10 10:52:06 +0100

    installer

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ sudo npm install gulp --save-dev
 sudo npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache --save-dev
 
 rm -r bower_components/
-
-rm bower.json 
+rm bower.json
+rm README.md
+rm instructions.md
 
 echo "Installation finished!"
